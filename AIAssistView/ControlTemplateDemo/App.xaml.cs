f5663433caf40981a00fc57862543707215a2a39
RavikumarAkuthotaSf4250 <152277499+RavikumarAkuthotaSf4250@users.noreply.github.com>
2025-04-22 18:07:26 +0530

    Updated App.xaml.cs file

--- AIAssistView/ControlTemplateDemo/App.xaml.cs
+++ AIAssistView/ControlTemplateDemo/App.xaml.cs
@@ -5,8 +5,11 @@
         public App()
         {
             InitializeComponent();
+        }
 
-            MainPage = new ControlTemplateDemo();
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new ControlTemplateDemo());
         }
     }
 }
